c0290e52d693299577bedc634566ba44cad5c255
David Dionisio <ddionisio@renegadeware.com>
2019-07-31 10:54:01 -0400

    initial commit

---
+++ Assets/LoL/PlayMakerExt/Actions/SignalBooleanWait.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.LoL {
+    [ActionCategory("Legends of Learning")]
+    public class SignalBooleanWait : FsmStateAction {
+        [ObjectType(typeof(SignalBoolean))]
+        [RequiredField]
+        public FsmObject signal;
+
+        public FsmEvent trueEvent;
+        public FsmEvent falseEvent;
+
+        private SignalBoolean mSignal;
+
+        public override void OnEnter() {
+            mSignal = (SignalBoolean)signal.Value;
+            mSignal.callback += OnSignal;
+        }
+
+        public override void OnExit() {
+            if(mSignal)
+                mSignal.callback -= OnSignal;
+        }
+
+        void OnSignal(bool b) {
+            Fsm.Event(b ? trueEvent : falseEvent);
+            Finish();
+        }
+    }
+}
