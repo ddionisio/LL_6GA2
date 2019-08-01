573bbcf83ee1365485e91a7ef8bcfa5aa75ad21b
David Dionisio <ddionisio@renegadeware.com>
2019-08-01 17:40:40 -0400

    some preps

---
+++ Assets/Scripts/Game/CameraOrient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrient : MonoBehaviour {
+
+    public Transform defaultTarget;
+    public float distance;
+    public Vector3 startRotation; //rotate from target to camera
+    public M8.RangeFloat pitchRange; //angle limit for pitch rotation
+
+    private Transform mTarget;
+
+    public void SetTarget(Transform t) {
+        mTarget = t;
+
+        //move camera towards target
+    }
+
+    void OnEnable() {
+        
+    }
+}
