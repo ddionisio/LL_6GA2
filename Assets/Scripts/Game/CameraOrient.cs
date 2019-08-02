b539b35083f17989b9dcad673ca01c6e3c9c7059
David Dionisio <ddionisio@renegadeware.com>
2019-08-02 18:33:12 -0400

    some setup for camera orient

--- Assets/Scripts/Game/CameraOrient.cs
+++ Assets/Scripts/Game/CameraOrient.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraOrient : MonoBehaviour {
+    public Transform target;
 
-    public Transform defaultTarget;
+    //NOTE: starting from back dir of look-at
+    public Transform defaultLookAt;
     public float distance;
     public Vector3 startRotation; //rotate from target to camera
     public M8.RangeFloat pitchRange; //angle limit for pitch rotation
+    public float yDistance; //move as pitch change
 
-    private Transform mTarget;
+    public float yawScale; //scale from yaw-delta provided
 
-    public void SetTarget(Transform t) {
-        mTarget = t;
+    public float gotoDelay; //delay when going to new target
+    public float moveDelay; //delay when moving
+
+    public Transform lookAt { get; private set; }
+
+    public bool isBusy { get { return mRout != null; } }
+
+    private Vector3 mDestForward;
+    private Vector3 mDestPosition;
+
+    private Coroutine mRout;
+
+    public void SetTarget(Transform t, bool immediate) {
+        lookAt = t;
 
         //move camera towards target
     }
